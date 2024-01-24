9434175805fa35ba27af4c75b828db9b8151c619
tansubva <guest270600@gmail.com>
2024-01-24 09:13:39 -0500

    check

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,38 +6,29 @@ const Calculator = () => {
     const [firstNum,setFirstNum] = useState(0)
     const [secondNum,setSecondNum] = useState(0)
 
-    const handleAddFirst = (e) => {
-        const newNum = firstNum++
-        setFirstNum(newNum)
+    const handleNum = (id) => {
+        setFirstNum(id)
     }
-    const handleRemoveFirst = () => {
-
-    }
-
-    const handleAddSecond = () => {
-
-    }
-    const handleRemoveSecond = () => {
-
-    }
+    
 
     
 
     return(
         <div className="calculator">
-            <div id="output">{firstNum, operator, secondNum}</div>
+            <div id="output">{result}</div>
+            {/* <div style={"display: flex"}></div> */}
 
             <div className="containerCalculator">
                 <div id="buttons">
-                    <button id="1">1</button>
-                    <button id="2">2</button>
-                    <button id="3">3</button>
-                    <button id="4">4</button>
-                    <button id="5">5</button>
-                    <button id="6">6</button>
-                    <button id="7">7</button>
-                    <button id="8">8</button>
-                    <button id="9">9</button>
+                    <button onClick={() => {handleNum(1)}} id="1">1</button>
+                    <button onClick={() => {handleNum(2)}} id="2">2</button>
+                    <button onClick={() => {handleNum(3)}} id="3">3</button>
+                    <button onClick={() => {handleNum(4)}} id="4">4</button>
+                    <button onClick={() => {handleNum(5)}} id="5">5</button>
+                    <button onClick={() => {handleNum(6)}} id="6">6</button>
+                    <button onClick={() => {handleNum(7)}} id="7">7</button>
+                    <button onClick={() => {handleNum(8)}} id="8">8</button>
+                    <button onClick={() => {handleNum(9)}} id="9">9</button>
                 </div>
                 <div id="operators">
                     <button id="plus">+</button>
